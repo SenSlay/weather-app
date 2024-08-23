bdd158df29b3a49226b54829d90a76cab04c8f85
Irvan Castro <castroirvan@gmail.com>
2024-08-23 16:12:44 +0800

    create api and dom js files rename updateDom() to renderForecastData() create getWeatherIcon() to display right icon for condition make displayLoader function include other weather condition modify class and id names

---
+++ src/api.js
@@ -0,0 +1,36 @@
+// Get weather forecast data on a location by using weather.visualcrossing.com API
+async function getLocationForecastData(location) {
+    // Promise implementation
+    /* 
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=FNMEC42CA9CLAVPZ58JSRLYG8`, {mode: 'cors'})
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        }); 
+    */
+
+    // Async/Await Implementation
+    try {
+        const APIkey = 'FNMEC42CA9CLAVPZ58JSRLYG8'
+        const response =  await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=metric&key=${APIkey}`, { mode: 'cors' });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        const forecastData = await response.json();
+        console.log(forecastData);
+        return forecastData;
+    } catch (error) {
+        console.log('Location not found');
+    }
+}
+
+export default getLocationForecastData;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,6 @@
 import "./style.css";
-
-async function getLocationData(city) {
-    // Promise implementation
-    // fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=FNMEC42CA9CLAVPZ58JSRLYG8`, {mode: 'cors'})
-    //     .then(function(response) {
-    //         if (!response.ok) {
-    //             throw new Error('Network response was not ok');
-    //         }
-    //         return response.json();
-    //     })
-    //     .then(data => {
-    //         console.log(data);
-    //     })
-    //     .catch(error => {
-    //         console.error('There was a problem with the fetch operation:', error);
-    //     });
-
-    // Async/Await Implementation
-    try {
-        const response =  await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=FNMEC42CA9CLAVPZ58JSRLYG8`);
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.log('City not valid');
-    }
-}
-
-function updateDom(data) {
-    const locationHeader = document.getElementById('location-header');
-    locationHeader.textContent = data.resolvedAddress;
-
-    const humidityP = document.getElementById('humidity');
-    humidityP.textContent = data.currentConditions.humidity;
-
-    console.log(locationHeader);
-}   
+import getLocationForecastData from "./api";
+import renderForecastData, {displayLoader} from "./dom";
 
 const form = document.getElementById('form');
 
@@ -44,8 +9,11 @@ form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     // Get value from user input and use function to get data
-    const city = target.querySelector('#city').value;
-    const data = await getLocationData(city);
+    const location = target.querySelector('#location-input').value;
+    
+    // Display loader as we wait to receive the data from the API
+    displayLoader();
+    const data = await getLocationForecastData(location);
 
-    updateDom(data);
+    renderForecastData(data);
 });
